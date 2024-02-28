888e52fc3b09c712918116e178f564881d578f1e
kweku kankam <kwekukankam@gmail.com>
2024-02-28 03:58:29 +0000

    ghanged crypto formulae

--- tusserver/server.js
+++ tusserver/server.js
@@ -4,7 +4,7 @@ const {FileStore} = require('@tus/file-store');
 const tusServer = new Server({
   path: '/files',
   async namingFunction(req,{filename}){
-    const id = crypto.randomBytes(16).toString('hex')
+    const id = crypto.lib.WordArray.random(8);//crypto.randomBytes(16).toString('hex')
     return `${id}.${filename}`;
   },
   datastore: new FileStore({ directory: './files' })
