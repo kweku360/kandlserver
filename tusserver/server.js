2c6503553675694a1f77cdc4fda140a5b2fe6cd8
kweku kankam <kwekukankam@gmail.com>
2024-02-28 04:08:43 +0000

    changed crypto

--- tusserver/server.js
+++ tusserver/server.js
@@ -4,10 +4,22 @@ const {FileStore} = require('@tus/file-store');
 const tusServer = new Server({
   path: '/files',
   async namingFunction(req,{filename}){
-    const id = crypto.lib.WordArray.random(8);//crypto.randomBytes(16).toString('hex')
+    const id = makeid(16);//crypto.randomBytes(16).toString('hex')
     return `${id}.${filename}`;
   },
   datastore: new FileStore({ directory: './files' })
 })
+
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
 
 module.exports = tusServer;
